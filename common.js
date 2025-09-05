d9e914fc41ee6f88cd2d7c70e0a2a813ec5d78c1
haidhuat <lastotejhonson@gmail.com>
2025-09-05 11:45:25 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="";
+		var url="https://yogaalliance.org";
 		window.location=url;
 	}
 }
@@ -11,4 +11,5 @@ var num=0;
 function getLoad()
 {
 	num++;
+
 }
